4f6789539d85f97a50dae8b066e434d39b2735d7
Skinnix <skinnix@gmail.com>
2024-10-26 15:51:11 +0200

    UI-Verbesserungen; Publish-Skript

---
+++ Tools/Publish.bat
@@ -0,0 +1,15 @@
+@echo off
+
+cd ..\Code
+
+dotnet restore ClientWeb\RhymeTool.Client.Web.csproj
+dotnet publish ClientWeb\RhymeTool.Client.Web.csproj -c Release -o ..\Build\Publish\
+
+dotnet restore App\RhymeTool.MauiBlazor.csproj
+dotnet publish App\RhymeTool.MauiBlazor.csproj -c Release -f net9.0-android -o
+
+copy /B /Y ..\Build\Release\net9.0-android\Net.Skinnix.RhymeTool.MauiBlazor-Signed.apk ..\Build\Publish\wwwroot\app.apk
+
+pause
+
+explorer.exe "%~dp0..\Build\Publish\wwwroot\"
